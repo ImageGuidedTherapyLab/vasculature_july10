fff385ca59ac3274edb85626864eb08d841c50ea
David Fuentes <fuentesdt@gmail.com>
2011-07-11 17:50:48 -0500

    initial directions for carlos

--- treatmentPlanning.py
+++ treatmentPlanning.py
@@ -1,10 +1,12 @@
 """
-treatment planning model 
+treatment planning model for RF ablation
 """
 # import needed modules
 import petsc4py, numpy, sys
 PetscOptions =  sys.argv
 PetscOptions.append("-ksp_monitor")
+PetscOptions.append("-ksp_rtol")
+PetscOptions.append("1.0e-15")
 #PetscOptions.append("-help")
 petsc4py.init(PetscOptions)
 
@@ -16,20 +18,13 @@ petscRank = PETSc.COMM_WORLD.getRank()
 petscSize = PETSc.COMM_WORLD.Get_size()
 sys.stdout.write("petsc rank %d petsc nproc %d\n" % (petscRank, petscSize))
 
-# break up processors into communicators
-NumProcsPerSubComm = 4
-color = petscRank/NumProcsPerSubComm 
-NumSubCommunicators = petscSize / NumProcsPerSubComm + 1
-subcomm = PETSc.Comm(MPI.COMM_WORLD.Split(color))
-subRank = subcomm.Get_rank()
-subSize = subcomm.Get_size()
-sys.stdout.write("number of sub communictors %d \n" % ( NumSubCommunicators ))
-sys.stdout.write("subcomm rank %d subcomm nproc %d\n" % (subRank, subSize))
-
 # set shell context
+# TODO import vtk should be called after femLibrary ???? 
+# FIXME WHY IS THIS????
 import femLibrary
-fem = femLibrary.PyFEMInterface(4.0) 
-fem.SetuplibMesh( subcomm ) # initialize libMesh data structures
+# initialize libMesh data structures
+libMeshInit = femLibrary.PyLibMeshInit(PetscOptions,PETSc.COMM_WORLD) 
+  
 
 # inifile = None ==> setup from command line
 inifile=None
@@ -38,37 +33,50 @@ fem.SetupIni( inifile )
 fem.SetIniValue( "optical/mu_a_healthy","4.0" ) 
 fem.SetIniValue( "optical/mu_a_tumor","400.0" ) 
 
+# store control variables
+getpot = femLibrary.PylibMeshGetPot(PetscOptions) 
+# from Duck table 2.15
+getpot.SetIniValue( "material/specific_heat","3840.0" ) 
+# set ambient temperature 
+getpot.SetIniValue( "initial_condition/u_init","21.0" ) 
+# from Duck
+getpot.SetIniValue( "electric_conductivity/s_0_healthy", "6.0") 
+
 # initialize FEM Mesh
-# must setup Ini File first
-fem.SetupUnStructuredGrid( "phantomMesh.e",0 ) 
-#fem.SetupStructuredGrid( (10,10,4) ,[0.0,1.0],[0.0,1.0],[0.0,1.0]) 
+femMesh = femLibrary.PylibMeshMesh()
+#Setup Affine Transformation for registration
+RotationMatrix = [[1.,0.,0.],
+                  [0.,1.,0.],
+                  [0.,0.,1.]]
+Translation =     [0.,0.,0.]
+# TODO input full path to FEM mesh here
+femMesh.SetupUnStructuredGrid( "clusterVessel.e",0,RotationMatrix, Translation  ) 
+MeshOutputFile = "fem_data.e" 
 
 # add the data structures for the Background System Solve
 # set deltat, number of time steps, power profile, and add system
-deltat = 5.0
-ntime  = 7 
-fem.AddPennesSystem(2,deltat,ntime,[[1,28,46,78,119],[0.0,4.0,0.0,9.0,0.0]]) 
+nsubstep = 1
+acquisitionTime = 5.00
+deltat = acquisitionTime / nsubstep
+ntime  = 60 
+eqnSystems =  femLibrary.PylibMeshEquationSystems(femMesh,getpot)
+getpot.SetIniPower(nsubstep,  [ [1,5,41,ntime],[1.0,0.0,1.0,0.0] ])
+eqnSystems.AddPennesRFSystem("StateSystem",deltat,ntime) 
 
 # initialize libMesh data structures
-fem.InitializeEquationSystems( ) 
+eqnSystems.init( ) 
 
 # print info
-fem.printSelf() 
+eqnSystems.PrintSelf() 
+
+# write IC
+exodusII_IO = femLibrary.PylibMeshExodusII_IO(femMesh)
+exodusII_IO.WriteTimeStep(MeshOutputFile,eqnSystems, 1, 0.0 )  
 
 # loop over time steps and solve
 for timeID in range(1,ntime):
-   fem.UpdateTransientSystemTimeStep("StateSystem",timeID ) 
-   fem.SystemSolve( "StateSystem" ) 
-   #fem.StoreTransientSystemTimeStep("StateSystem",timeID ) 
-   fem.WriteTimeStep("fem_data.e" , timeID, timeID )  
-
-
-
-
-
-
-
-
-
-
-
+   print "time step = " ,timeID
+   eqnSystems.UpdateTransientSystemTimeStep("StateSystem",timeID ) 
+   eqnSystems.SystemSolve( "StateSystem" ) 
+   #eqnSystems.StoreTransientSystemTimeStep("StateSystem",timeID ) 
+   exodusII_IO.WriteTimeStep(MeshOutputFile,eqnSystems, timeID+1, timeID*deltat )  
